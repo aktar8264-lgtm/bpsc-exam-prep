3a4eada1a6c467c84f973fd5c28705cf1dbd4848
aktar8264-lgtm <aktar8264@gmail.com>
2026-05-05 13:01:32 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,2 @@
-// অনলাইন-নির্ভর অ্যাপের জন্য Service Worker
-// এই ফাইলটি পুশ নোটিফিকেশন গ্রহণ করার জন্য প্রয়োজন।
-
-self.addEventListener('install', () => {
-  console.log('Service Worker: I am installed and ready!');
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('Service Worker: I am active and taking control!');
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  // অনলাইন-নির্ভর অ্যাপের জন্য আমরা নেটওয়ার্ক অনুরোধে হস্তক্ষেপ করব না।
-  // ব্রাউজার সরাসরি ইন্টারনেট থেকে সব আনবে।
-  return;
-});
+// OneSignal এর Service Worker ফাইলের সাথে কাজ করার জন্য ইম্পোর্ট করা হচ্ছে
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
